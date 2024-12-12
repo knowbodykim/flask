bee78201bcb64157e3f2e75d271ee49498f3f58d
knowbodykim <knowbodykim@gmail.com>
2024-12-12 13:30:56 +0900

    DB 연동

---
+++ pybo/views/main_views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+
+bp = Blueprint('main', __name__, url_prefix='/')
+# 밑에 접속하는 주소의 기본값
+# 예) url_prefix = '/main'
+# localhost:5000/
+
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+
+@bp.route('/')
+def index():
+    return 'Pybo index'
